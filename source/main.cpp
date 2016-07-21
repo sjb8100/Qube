2403d58235f731213daf2dd4743e1e01bee4a321
Steve <steven.g.ball@gmail.com>
2016-07-20 22:19:31 -0400

    Initial github commit

---
+++ source/main.cpp
@@ -0,0 +1,20 @@
+// ******************************************************************************
+// Filename:    main.cpp
+// Project:     Qube
+// Author:      Steven Ball
+//
+// Purpose:
+//   The main entry point for the application. Creates the initial instances of
+//   the container classes, iterates the game loop, polls events/input and 
+//   detects for game closure to cleanup the with the destruction.
+// 
+// Revision History:
+//   Initial Revision - 20/07/16
+//
+// Copyright (c) 2005-2016, Steven Ball
+// ******************************************************************************
+
+
+int main(void)
+{
+}
